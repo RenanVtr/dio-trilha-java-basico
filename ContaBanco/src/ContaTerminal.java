143c087dbef84099ff6ef2f7f04174fd60d90be7
RenanVtr <renanvitorg@hotmail.com>
2023-05-30 16:51:11 -0300

    Scanner e captura dos valores de variaveis

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite a agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo da conta: ");
+        float saldo = scanner.nextFloat();
+
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco. Sua agência é "+agencia+", conta "+numeroConta+" e seu saldo de R$ "+saldo+" já está disponível.");
+
+
+
+        
     }
 }
