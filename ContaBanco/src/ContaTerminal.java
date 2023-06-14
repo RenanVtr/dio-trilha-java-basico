77deab9293a4b195a1a90fae725a32a834d7f413
RenanVtr <renanvitorg@hotmail.com>
2023-06-14 09:17:35 -0300

    Entrega do desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,23 +4,22 @@ import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite o número da conta: ");
+            int numeroConta = scanner.nextInt();
+
+            System.out.println("Digite a agência: ");
+            String agencia = scanner.next();
+
+            System.out.println("Digite seu nome: ");
+            String nomeCliente = scanner.next();
+
+            System.out.println("Digite o saldo da conta: ");
+            float saldo = scanner.nextFloat();
 
 
-        System.out.println("Digite o número da conta: ");
-        int numeroConta = scanner.nextInt();
-
-        System.out.println("Digite a agência: ");
-        String agencia = scanner.next();
-
-        System.out.println("Digite seu nome: ");
-        String nomeCliente = scanner.next();
-
-        System.out.println("Digite o saldo da conta: ");
-        float saldo = scanner.nextFloat();
-
-
-        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco. Sua agência é "+agencia+", conta "+numeroConta+" e seu saldo de R$ "+saldo+" já está disponível.");
+            System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco. Sua agência é "+agencia+", conta "+numeroConta+" e seu saldo de R$ "+saldo+" já está disponível.");
+        }
 
 
 
