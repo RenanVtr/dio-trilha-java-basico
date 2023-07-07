6150a44b4cee9a643ce771009eec9989869c59ff
RenanVtr <renanvitorg@hotmail.com>
2023-07-07 13:29:26 -0300

    Implementação da logica de seleção de candidados

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -5,9 +5,10 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         
-        analisarCandidato(1900);
-        analisarCandidato(2200);
-        analisarCandidato(2000);
+        //analisarCandidato(1900);
+        //analisarCandidato(2200);
+        //analisarCandidato(2000);
+        selecaoCandidatos();
     }
 
     static void selecaoCandidatos(){
@@ -17,10 +18,21 @@ public class ProcessoSeletivo {
 
         int candidatosSelecionados = 0;
         int candidatoAtual = 0;
+        double salarioBase= 2000;
 
-        while (candidatosSelecionados < 5){
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+                String candidato = candidatos[candidatoAtual];
+                double salarioPretendido = valorPretendido();
 
-            
+                System.out.println("O candidato "+ candidato + " solicitou o salário de R$ " + salarioPretendido);
+                if (salarioBase >= salarioPretendido ){
+                    System.out.println("O candido "+ candidato + " foi selecionado para a vaga.");
+                    candidatosSelecionados++;
+                }else{
+                    System.out.println("O candidato " + candidato + " não foi selecionado para a vaga.");
+                    }
+                    candidatoAtual++;
+
         }
 
     }
