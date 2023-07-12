4b543a552600e537054c0ef55a8e2192d40a64b4
RenanVtr <renanvitorg@hotmail.com>
2023-07-12 16:34:41 -0300

    Criacao da logica de contagem

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*O sistema devera receber dois parametros via terminal representando dois numeros inteiros,
+         * e imprimir o numero de iteracoes entre os dois valores, contando do valor mais baixo para o mais alto
+         * Caso o segundo parametro for maior que o primeiro, o sistema devera lancar uma exception
+         */
+
+    public static void main(String[] args) {
+        
+         Scanner terminal = new Scanner(System.in);
+
+            //Captura de dados
+            System.out.println("Digite o primeiro parametro: ");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parametro: ");
+            int parametroDois = terminal.nextInt();
+            
+            
+            //Chamando metodo contador
+            contar(parametroUm, parametroDois);
+               
+
     }
+    
+
+    //Metodo contador
+    static void contar( int parametroUm, int parametroDois){
+            
+        //Calculando a diferença entre os valores
+        int  contagem = parametroDois - parametroUm;
+
+        //Imprimindo as iteracoes conforme regra de negocio
+        for (int iteracao = 1; iteracao <= contagem ; iteracao++ ){
+            System.out.println("Impriminido o numero "+ iteracao);
+        }
+    }
+
 }
---
+++ Contador/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException {
+
+    if (parametroUm > parametroDois)
+    {System.out.println("O segundo parametro deve ser maior que o primeiro.");}
+}
